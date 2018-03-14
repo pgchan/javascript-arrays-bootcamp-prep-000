8dacbf5b95100dd61539d137239a93b66dc5834c
P G <pgauchan@gmail.com>
2018-03-14 04:08:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,8 +14,12 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
+<<<<<<< HEAD
   array.push(element);
   return array;
+=======
+  return array.push(element);
+>>>>>>> 2f67d6d2568bf84c8a8d1797489cd1496cc20d63
 }
 
 function accessElementInArray(array, index) {
@@ -23,8 +27,12 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+<<<<<<< HEAD
   array.shift();
   return array;
+=======
+  return array.shift();
+>>>>>>> 2f67d6d2568bf84c8a8d1797489cd1496cc20d63
 }
 
 function removeElementFromBeginningOfArray(array) {
@@ -32,8 +40,12 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+<<<<<<< HEAD
   array.pop();
   return array;
+=======
+  return array.pop();
+>>>>>>> 2f67d6d2568bf84c8a8d1797489cd1496cc20d63
 }
 
 function removeElementFromEndOfArray(array) { 
